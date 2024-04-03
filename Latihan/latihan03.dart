743c58f0d86c6c62879edc18b3016e4e8b1254b0
Enrico244 <163961454+Enrico-22410100011@users.noreply.github.com>
2024-04-03 20:24:13 +0700

    Add files via upload

---
+++ Latihan/latihan03.dart
@@ -0,0 +1,28 @@
+void main()
+{
+  for (int i =1; i<=10;i++)
+    {
+      print(i);
+    }
+  int j = 1;
+  while(j<=5)
+  {
+    print(j);
+    j++;
+  }
+
+  int k = 1;
+  do
+  {
+    print(k);
+    k++;
+  } while(k<=5);
+}
+
+/*
+tugas02.dart
+tampilkan bilnagan ini genap atau gnajil dengan input n <100
+tugas03.dart
+tampilkan matrix '*' nxn
+input n = 2, n = 3
+ */
