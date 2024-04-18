f8007a2a2478359b75578de3240f6a41f83d7c5b
Enrico244 <163961454+Enrico-22410100011@users.noreply.github.com>
2024-04-18 10:52:55 +0700

    Add files via upload

---
+++ Pertemuan 4/Kedua/main.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp( MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Kedua',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+class _MyHomePageState extends State<MyHomePage> {
+  TextEditingController isi = TextEditingController();
+  TextEditingController hasil = TextEditingController();
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text('Flutter Kedua'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(15),
+        child: Column(
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.all(10),
+              margin: EdgeInsets.all(10),
+              child:TextField(
+                controller: isi,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Isi Nama',
+                  hintText: 'Ambatron'
+                ),
+              ),
+            ),
+
+            Row(
+              mainAxisAlignment:MainAxisAlignment.center,
+              children: [
+                Container(
+                  padding: EdgeInsets.all(10),
+                  margin: EdgeInsets.all(10),
+                  child:ElevatedButton(
+                  child: Text('Tampil'),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blue,
+                    elevation: 0
+                  ),
+                    onPressed: () {
+                      var isi1 = isi.text.toString();
+                      hasil.text = isi1.toString();
+                    },
+                  )
+            )
+            ],
+        ),
+
+      Container(
+        padding: EdgeInsets.all(10),
+        margin: EdgeInsets.all(10),
+        child:TextField( controller: hasil,
+          decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'Hasil'
+              ),
+            ),
+          ),],
+        ),
+      ),
+    );
+  }
+}
